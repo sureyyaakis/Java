1de0e604c6ab2d8b21c0c62a7afe17ea47cc4fd4
Sureyya Betul Akis <sureyyaabetul@gmail.com>
2018-02-25 22:17:20 -0600

    Create playMusic.java

---
+++ playMusic.java
@@ -0,0 +1,15 @@
+boolean playButton = true;
+
+    public void playMusic() {
+        if(playButton){
+            System.out.println("Music is playing");        
+        }else{
+            System.out.println("Music is paused");
+        }
+    
+    
+}
+// This should print out: "Music is playing" when playButton is true
+// and "Music is paused" when playButton is false.
+
+
