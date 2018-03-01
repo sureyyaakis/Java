2ec4769028146250c92b20303ffd8a4c5340b5d7
Sureyya Betul Akis <sureyyaabetul@gmail.com>
2018-03-01 17:20:02 -0600

    Create array.java

---
+++ array.java
@@ -0,0 +1,9 @@
+public class array {
+	public static void main (String[] args) {
+		
+		int myarray[]= {2, 4, 6, 7, 10}; // creating array
+		
+		System.out.println(myarray[0]); // index of my array
+	}
+
+}
