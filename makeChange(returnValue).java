aadedced792fd00275a2fc6a2cab9be0fe7a0cbb
Sureyya Betul Akis <sureyyaabetul@gmail.com>
2018-03-07 09:29:32 -0600

    Create makeChange(returnValue).java

---
+++ makeChange(returnValue).java
@@ -0,0 +1,9 @@
+// Remember to return a value you need 1) a return type and 2) a return statement
+
+public double makeChange(double itemCost, double dollarsProvided){
+    double change = dollarsProvided - itemCost; // Calculate and return the calculated change
+    return change;
+}
+
+
+
